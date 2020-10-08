389bd01d3cfc244af47da1e77a3114f4bd3d5a7c
atlfinesse <finesse.cap@gmail.com>
2020-10-08 11:57:47 -0700

    beginning pseudocode

---
+++ script.js
@@ -0,0 +1,17 @@
+document.body.addEventListener('click', (event) => {
+  console.log(event.target)
+})
+
+
+// Make variables for each square
+  // Make each square light up 
+  // Make squares light 
+// Make each square light up randomly
+// Make 
+// Need a right and wrong selection
+  // If correct --> repeat sequence and add another color
+  // Else wrong -->  end game
+
+
+
+// Make function to toggle on 'light up' class
