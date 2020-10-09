1d9114e16a04376cd18b8ecfe9be56fc69ad7004
atlfinesse <finesse.cap@gmail.com>
2020-10-09 14:12:57 -0700

    gettin organized

--- script.js
+++ script.js
@@ -16,65 +16,106 @@ let colorSwitcher = 0
 let round = 0
 /////// SIDE BUTTON VARIABLES
 const start = document.querySelector('#start')
+const restart = document.querySelector('#restart')
 
 
-// start logic
-start.addEventListener('click', () => {
-  play()
-})
+// start logic ***********
 
-// game run logic
-// setTimeout(() => {
-  
-// }, timeout);
-function play() {
+
+////////// GAME RUN LOGIC //////////
+///// FUNCTIONS THAT MAKE THE GAME WORK
+
+
+
+//******** need a function to start next round after player input
+
+
+
+// function to check if the player choice and game choice is equal
+function checkChoices(arr1, arr2) {
+  return arr1.toString() === arr2.toString()
+}
+// funciton to signify a new color and push that color to gameChoice array
+function newColor() {
   colorSwitcher = Math.random() * 100
   if (colorSwitcher < 25) {
-    blue.classList.add('lightUp')
-    setTimeout(dimBlue, 850)
+    signifyBlue()
     gameChoice.push('blue')
   } else if (colorSwitcher < 50) {
-    green.classList.add('lightUp')
-    setTimeout(dimGreen, 850)
+    signifyGreen()
     gameChoice.push('green')
   } else if (colorSwitcher < 75) {
-    red.classList.add('lightUp')
-    setTimeout(dimRed, 850)
+    signifyRed()
     gameChoice.push('red')
   } else if (colorSwitcher < 100) {
-    yellow.classList.add('lightUp')
-    setTimeout(dimYellow, 850)
+    signifyYellow()
     gameChoice.push('yellow')
   }
   console.log(gameChoice)
   return gameChoice
 }
-///// FUNCTIONS THAT MAKE THE GAME WORK
-// function to check if the player choice and game choice is equal
-function checkChoices(arr1, arr2) {
-  return arr1.toString() === arr2.toString()
+// function to signify whats already in the array without pushing anything
+function replayGameChoice() {
+  for (let i = 0; i < gameChoice.length; i++) {
+    console.log(gameChoice[i])
+    if (gameChoice[i] == 'blue') {
+      signifyBlue()
+    } else if (gameChoice[i] == 'green') {
+      signifyGreen()
+    } else if (gameChoice[i] == 'red') {
+      signifyRed()
+    } else if (gameChoice[i] == 'yellow') {
+      signifyYellow()
+    }
+  }
 }
-// functions to dim colors
-function dimBlue() {
-  blue.classList.remove('lightUp')
+// functions to light and dim colors
+function signifyBlue() {
+  blue.classList.add('lightUp')
+  setTimeout(function () {
+    blue.classList.remove('lightUp')
+  }, 1500)
 }
-function dimGreen() {
-  green.classList.remove('lightUp')
+
+function signifyGreen() {
+  green.classList.add('lightUp')
+  setTimeout(function () {
+    green.classList.remove('lightUp')
+  }, 1500)
 }
-function dimRed() {
-  red.classList.remove('lightUp')
+
+function signifyRed() {
+  red.classList.add('lightUp')
+  setTimeout(function () {
+    red.classList.remove('lightUp')
+  }, 1500)
 }
-function dimYellow() {
-  yellow.classList.remove('lightUp')
+
+function signifyYellow() {
+  yellow.classList.add('lightUp')
+  setTimeout(function () {
+    yellow.classList.remove('lightUp')
+  }, 1500)
 }
-//function to restart the game
-function restart() {
-  gameChoice = []
-  playerChoice = []
-}
+
+//function to restart the game ************
+// function reset() {
+//   gameChoice = []
+//   playerChoice = []
+// }
 
 
 ///// EVENT LISTENERS
+// function to start new game
+start.addEventListener('click', () => {
+  newColor()
+})
+// restart game
+// restart.addEventListener('click', () => {
+//   reset()
+//   console.log(gameChoice)
+// })
+
 // player click action event listener
 container.addEventListener('click', (event) => {
   playerChoice.push(event.target.id)
@@ -113,10 +154,6 @@ container.addEventListener('click', (event) => {
 //   setTimeout(play, 1000 * i, gameChoice[i])
 // }
 
-
-// let newRound = setTimeout(play, 2000)
-
-
 function go() {
 for (let i = 0; i <= round; i++) {
   console.log(gameChoice)
@@ -127,39 +164,17 @@ for (let i = 0; i <= round; i++) {
   console.log(`round ${round}`)
 }
 
-/////// EXTRA LOOSE ENDS
-// make sure you can't click score becasue that will push an empty string into the arry of playerChoice
-
-function replayGameChoice() {
-  // tell me whats already in the array without pushing anything
-  // light up whats in the array
-
-  for (let i = 0; i < gameChoice.length; i++) {
-    console.log(gameChoice[i])
-    if (gameChoice[i] == 'blue') {
-        console.log('it was blue')
-      blue.classList.add('lightUp')
-      setTimeout(dimBlue, 850)
-    } else if (gameChoice[i] == 'green') {
-        console.log('it was green')
-      green.classList.add('lightUp')
-      setTimeout(dimGreen, 850)
-    } else if (gameChoice[i] == 'red') {
-      console.log('it was red')
-      red.classList.add('lightUp')
-      setTimeout(dimRed, 850)
-    } else if (gameChoice[i] == 'yellow') {
-      console.log('it was yellow')
-      yellow.classList.add('lightUp')
-      setTimeout(dimYellow, 850)
-    }
-  }
-}
-
-
 // game play function
   //first do replayGameChoice function(replay whatever the array already has and light them up)
   //then do play function(light up new color and add it to the array)
 
-  //lastly wait on player attempt to match array
+  //later do player click event lister (wait on player attempt to match array)
+    // player will click in a sequence and try to match
   //if player is correct redo game play function
+      //else game is over
+
+
+
+/////// EXTRA LOOSE ENDS
+// make sure you can't click score becasue that will push an empty string into the arry of playerChoice
+// ALL ******** NEED TO BE FIXED OR MOVED
